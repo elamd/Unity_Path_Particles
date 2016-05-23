504cadfe323342b298d05ac07b543e6830957b4e
Elam Daly <elam.d@iconinteractive.com>
2016-05-23 15:19:46 -0400

    Initial Commit

---
+++ Assets/Scripts/SplineParticleWalker.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineParticleWalker : MonoBehaviour{
+
+    public BezierSpline target;
+
+    void Update() {
+        Trail();
+    }
+
+    void Trail() {
+        ParticleSystem.Particle[] p = new ParticleSystem.Particle[GetComponent<ParticleSystem>().particleCount + 1];
+        int l = GetComponent<ParticleSystem>().GetParticles(p);
+    }
+
+}
