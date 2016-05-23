558fd1680ddc1aa53a2b9c282c9ec767ea940bfc
Elam Daly <elam.d@iconinteractive.com>
2016-05-23 18:27:42 -0400

    implementing spine particle following.

--- Assets/Scripts/SplineParticleWalker.cs
+++ Assets/Scripts/SplineParticleWalker.cs
@@ -12,6 +12,11 @@ public class SplineParticleWalker : MonoBehaviour{
     void Trail() {
         ParticleSystem.Particle[] p = new ParticleSystem.Particle[GetComponent<ParticleSystem>().particleCount + 1];
         int l = GetComponent<ParticleSystem>().GetParticles(p);
+
+        int i = 0;
+        while (i < l) {
+
+        }
     }
 
 }
