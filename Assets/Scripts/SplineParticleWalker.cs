00cdbf768d57640aa705c748118f7ffe8bf40163
Elam Daly <elam.d@iconinteractive.com>
2016-05-24 16:07:18 -0400

    Progress for new particle algorithm.

--- Assets/Scripts/SplineParticleWalker.cs
+++ Assets/Scripts/SplineParticleWalker.cs
@@ -3,20 +3,53 @@ using System.Collections;
 
 public class SplineParticleWalker : MonoBehaviour{
 
-    public BezierSpline target;
+    public BezierSpline spline;
+    public float duration;
+    private float progress;
+    public bool LookForward;
+    public SplineWalkerMode mode;
+    private bool goingForward = true;
 
+    void Start() {
+
+    }
+
+    // Update is called once per frame
     void Update() {
-        Trail();
-    }
-
-    void Trail() {
         ParticleSystem.Particle[] p = new ParticleSystem.Particle[GetComponent<ParticleSystem>().particleCount + 1];
-        int l = GetComponent<ParticleSystem>().GetParticles(p);
+        int particleCount = GetComponent<ParticleSystem>().GetParticles(p);
+        
+        for (int i = 0; i < particleCount - 1; i++) {
+            if (goingForward) {
+                progress += Time.deltaTime / duration;
+                if (progress > 1f) {
+                    if (mode == SplineWalkerMode.Once) {
+                        progress = 1f;
+                    }
+                    else if (mode == SplineWalkerMode.Loop) {
+                        progress -= 1f;
+                    }
+                    else {
+                        progress = 2f - progress;
+                        goingForward = false;
+                    }
+                }
+            }
+            else {
+                progress -= Time.deltaTime / duration;
+                if (progress < 0f) {
+                    progress = -progress;
+                    goingForward = true;
+                }
+            }
 
-        int i = 0;
-        while (i < l) {
+            float unitMeasure = p[i].startLifetime / spline.points.Length;
 
+            if (p[i].lifetime < p[i].startLifetime / spline.points.Length) {
+                p[i].velocity = spline.points.Length / p[i].startLifetime * spline.points[spline.points.Length - 1];
+            }
         }
+        GetComponent<ParticleSystem>().SetParticles(p, particleCount);
     }
-
+   
 }
